686c9408f298a7929cb1922ef265912f99064fd0
Ivanova Liliya <lelya211298@gmail.com>
2022-04-13 02:20:58 +0300

    added new routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from app.lesson2 import *
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    return render_hello_world()
+
+@app.route('/users/<username>')
+def user(username):
+    return show_user_profile(username)
+
+@app.route('/users/<username>/url')
+def redirect_user(username):
+    return redirect_page(username)

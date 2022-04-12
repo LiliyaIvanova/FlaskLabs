557a95c130decd346141bca228e12a77a4740877
Ivanova Liliya <lelya211298@gmail.com>
2022-04-13 02:13:07 +0300

    changed the code

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import os
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
+from app import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

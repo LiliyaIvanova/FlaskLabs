0f157f82889b3d7316cb21eea90a1cc19601b03d
Ivanova Liliya <lelya211298@gmail.com>
2022-04-13 02:18:38 +0300

    added new methods

--- app/lesson2.py
+++ app/lesson2.py
@@ -1,1 +1,10 @@
-from app import app
+from app import app, render_template
+
+def render_hello_world():
+    return render_template('hello_world.html')
+
+def show_user_profile(username):
+    return 'Hello, %s' % username
+
+def redirect_page(username):
+    return render_template('redirect_to_user.html', username=username)
